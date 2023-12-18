9c9f25dd0421a097bad021f71121a8c2c031bbcf
abhar89 <142299700+abhar89@users.noreply.github.com>
2023-12-18 23:41:15 +0800

    Update test_loading.py

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -4,5 +4,5 @@ import os
 def test_loading():
     for json_file in os.listdir('./examples'):
         if json_file.endswith('.json'):
-            flow = load_flow_from_json(f'./examples/{json_file}', build=False)
+            flow = load_flow_from_json(f'./examples/{AI_chatbot}', build=False)
             assert flow is not None
